97e7197ac550464c4f37574690541b3b31bc4b29
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-01-10 17:16:15 -0500

    added templates, views, urls

--- books/users/forms.py
+++ books/users/forms.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+# User = get_user_model()
+# users = User.objects.all()
+# print(users)
+
 
 class CustomUserCreationForm(UserCreationForm):
 
