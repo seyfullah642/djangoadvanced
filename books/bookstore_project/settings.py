3710dfafa950b075a63ec1af09aa805d620e8ea5
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-01-28 20:30:19 -0500

    added environ vars

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pjbq1eg5ufh($**$fmyqnicayb@7=x3ywn&u0g44!pz_=)9$_f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', defualt=0))
 
 ALLOWED_HOSTS = []
 
