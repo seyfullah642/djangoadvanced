19aed563dd42b79e5173f2a67506e65fcd529d7c
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-02-09 20:37:38 -0500

    included tests

--- books/books/models.py
+++ books/books/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.urls import reverse
 
@@ -7,6 +8,7 @@ class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
+        editable = False,
     )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
