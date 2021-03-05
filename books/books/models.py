fad7e003847689f7e84f07acd9c4a18c2ec0536e
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-03-04 19:08:37 -0500

    custom permission

--- books/books/models.py
+++ books/books/models.py
@@ -17,6 +17,13 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='covers/', blank=True)
 
 
+    class Meta:
+
+        permissions = [
+            ('special_status', 'Can read all books')
+        ]
+
+
     def __str__(self):
         return self.title
 
