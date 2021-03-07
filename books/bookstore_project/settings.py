1ec14b6b84eb19bfa03bb5ea86928803e5e35f0f
Sinan Ramazanoglu <sinanr86@gmail.com>
2021-03-06 20:27:00 -0500

    added stripe

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -170,3 +170,6 @@ DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STRIPE_TEST_PUBLISHABLE_KEY = os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
+STRIPE_TEST_SECRET_KEY = os.environ.get('STRIPE_TEST_SECRET_KEY')
